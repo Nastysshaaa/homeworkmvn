010b6b82119f8cadfb76d8c3ba7c072324522fda
Anastasiia <anastasia994@yandex.ru>
2023-02-27 16:49:11 +0500

    Changed file

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,7 +1,6 @@
-package ru.netology.javaqa;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import ru.netology.javaqa.BonusService;
 //import ru.netology.javaqa;
 
 public class BonusServiceTest {
